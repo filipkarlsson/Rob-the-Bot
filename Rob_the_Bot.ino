5a3935e093e837a7850fc9b9b0bb4e4bc720d522
Filip Karlsson <karlsson@filips.se>
2014-04-23 00:22:15 +0200

    Start by IR-controll, correct integration of analog IR sensors and some minor fixes to the code.

--- Rob_the_Bot.ino
+++ Rob_the_Bot.ino
@@ -1,6 +1,10 @@
+#include <DistanceGP2Y0A21YK.h>
 #include <QTRSensors.h>
 #include <Servo.h> 
-#include <DistanceGP2Y0A21YK.h> //library can be found here https://github.com/jeroendoggen/Arduino-distance-sensor-library
+
+
+//Edge sensors------------
+
 // This example is designed for use with eight QTR-1RC sensors or the eight sensors of a
 // QTR-8RC module.  These reflectance sensors should be connected to digital inputs 3 to 10.
 // The QTR-8RC's emitter control pin (LEDON) can optionally be connected to digital pin 2, 
@@ -13,11 +17,9 @@
 // monitor as numbers from 0 (maximum reflectance) to 2500 (minimum reflectance).
 
 
-#define NUM_SENSORS   4     // number of QTR sensors used
+#define NUM_SENSORS   4     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
 #define EMITTER_PIN   QTR_NO_EMITTER_PIN     // emitter is controlled by digital pin 2
-#define NUM_ANALOG    5     //number of sharp analog sensors
-
 
 // sensors 0 through 1 are connected to digital pins 3 through 4, respectively
 QTRSensorsRC qtrrc((unsigned char[]) {3, 4, 5, 6},
@@ -29,147 +31,162 @@ boolean out_front_left = false;
 boolean out_back_right = false; 
 boolean out_back_left = false; 
 
+//-----------------------
+
+//  Analog distance sensors----
+DistanceGP2Y0A21YK left;
+DistanceGP2Y0A21YK front_left;
+DistanceGP2Y0A21YK front_right;
+DistanceGP2Y0A21YK right;
+DistanceGP2Y0A21YK back;
+
+int d_left, d_front_left, d_front_right, d_right, d_back;
+
+
+// Start module ------
+
+int start_pin = 7;
+
+
+//---------------------
+
 Servo motor_right;
 Servo motor_left;
+
+int forward = 110; 
+int backward = 70;
+int still = 90;
+
 Servo servo;
 int servo_down = 161;
 int servo_up = 40;
-
-
-
-DistanceGP2Y0A21YK dist;
-int distance[NUM_ANALOG];
-
 
 void setup()
 {
   servo.attach(9);  // attaches the servo on pin 9 to the servo object
   motor_right.attach(11);
   motor_left.attach(10);
-  Serial.begin(9600);
-  dist.begin(A0);
-  dist.begin(A1);
-  dist.begin(A2);
-  dist.begin(A3);
-  dist.begin(A4);
+  
+  pinMode(start_pin, INPUT);
+  
+  left.begin(A0);
+  front_left.begin(A1);
+  front_right.begin(A2);
+  right.begin(A3);
+  back.begin(A4);
+    
 }
-
 
 
 void loop()
 {
-  delay(2000);
+  while(!digitalRead(start_pin))
+  {
+    delay(1);
+  }
+  
   servo.write(servo_down);
   delay(1500);
   
-  motor_right.write(100);
-  motor_left.write(100);
-  delay(5000);
-  motor_right.write(75);
-  motor_left.write(115);
+  motor_right.write(forward);
+  motor_left.write(backward);
+  delay(4000);
+  motor_right.write(forward);
+  motor_left.write(forward);
   
   while(true){
   // read raw sensor values
-  qtrrc.read(sensorValues);
-
-  //will make sure that all distance are equal to 0 again
-  for(int i = 0, i<5, i++){
-   distance[i] = 0;
-  }
-   //reads the distances in centimeter
-  dist.getDistanceCentimeter(distance);
-  
-  
-  //tactics for different distance values  
-  if(distance[0] > 0 and distance[1] > 0 and sensorValues > 1000){
-  motor_right.write(0);
-  motor_left.write(180);
-  delay(300);
-  }
-  else if(distance[0] > 0 and sensorValues > 1000){
-  motor_right.write(145);
-  motor_left.write(145);
-  delay(300);
-  motor_right.write(180);
-  motor_left.write(0);
-  }
-   else if(distance[1] > 0 and sensorValues > 1000){
-  motor_right.write(45);
-  motor_left.write(45);
-  delay(300); 
-  motor_right.write(180);
-  motor_left.write(0);
-  }
-    else if(distance[2] > 0 and sensorValues > 1000){
-  motor_right.write(180);
-  motor_left.write(180);  
-  delay(300); 
-  motor_right.write(180);
-  motor_left.write(0);
-  }
-   else if(distance[3] > 0 and sensorValues > 1000){
-  motor_right.write(0);
-  motor_left.write(0); 
-  delay(300);
-  motor_right.write(180);
-  motor_left.write(0);
-  
-  }
-  else if (distance[4] > 0 and sensorValues > 1000){
-   motor_right.write(180);
-  motor_left.write(0);
-  delay(300);
-  motor_right.write(180);
-  motor_left.write(180);    
-  }
-  
-  // print the sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
-  // 1023 means minimum reflectance
-  
-    if(sensorValues[0] < 1000){
-      out_front_left = true;   
-    }
-    if(sensorValues[1] < 1000){
-      out_front_right = true;   
-    }
-    if(sensorValues[2] < 1000){
-      out_back_left = true;   
-    }
-    if(sensorValues[3] < 1000){
-      out_back_right = true;   
-    }
+      qtrrc.read(sensorValues);   
     
+      // sensor values as numbers from 0 to 2500, where 0 means maximum reflectance and
+      // 1023 means minimum reflectance
+      
+        if(sensorValues[0] < 1000){
+          out_front_left = true;   
+        }
+        if(sensorValues[1] < 1000){
+          out_front_right = true;   
+        }
+        if(sensorValues[2] < 1000){
+          out_back_left = true;   
+        }
+        if(sensorValues[3] < 1000){
+          out_back_right = true;   
+        }
+        
+        
+  //read distance sensors
+      d_left = left.getDistanceCentimeter();
+      d_front_left = front_left.getDistanceCentimeter();
+      d_front_right = front_right.getDistanceCentimeter();
+      d_right = right.getDistanceCentimeter();
+      d_back = back.getDistanceCentimeter();
+      
+  //--------------------------------------------------------------------
+
   
-  if(out_front_left){
-    motor_right.write(100);
-    motor_left.write(40);
-    delay(300);
-    out_front_left = false;
-  }
-   else if(out_front_right){
-    motor_right.write(120);
-    motor_left.write(80);
-    delay(300);
-    out_front_right = false;
-  }
-     else if(out_back_left){
-    motor_right.write(80);
-    motor_left.write(100);
-    delay(300);
-    out_back_left = false;
-  }
-     else if(out_back_right){
-    motor_right.write(80);
-    motor_left.write(100);
-    delay(300);
-    out_back_right = false;
-  }
-  
-  else{
-  motor_right.write(75);
-  motor_left.write(113);
-  }
-  
+      
+  // Action :D --------------------
+      //----- edge ----
+      if(out_front_left){
+        motor_right.write(backward);
+        motor_left.write(backward - 10); //faster
+        delay(300);
+        out_front_left = false;
+      }
+       else if(out_front_right){
+        motor_right.write(backward - 10); //faster
+        motor_left.write(backward);
+        delay(300);
+        out_front_right = false;
+      }
+         else if(out_back_left){
+        motor_right.write(forward);
+        motor_left.write(forward);
+        //delay(300);
+        out_back_left = false;
+      }
+         else if(out_back_right){
+        motor_right.write(forward);
+        motor_left.write(forward);
+        //delay(300);
+        out_back_right = false;
+      }
+      
+      else{
+      motor_right.write(forward);
+      motor_left.write(forward);
+      }
+      
+      
+      
+     //------------ opponent---------
+ 
+     if((d_front_left < 70) && (d_right < 70)){  //If opponent is right infront go forward as long as opponent is in right in front
+       while((d_front_left < 70) && (d_right < 70)){
+         motor_right.write(forward);
+         motor_left.write(forward);
+       }
+     }
+     
+     if(d_left < 70){
+     motor_right.write(forward);
+     motor_left.write(backward);
+     delay(500);
+     }
+     
+     if(d_right < 70){
+     motor_right.write(backward);
+     motor_left.write(forward);
+     delay(500);
+     }
+     
+     if(d_back < 70){
+     motor_right.write(forward);
+     motor_left.write(backward);
+     delay(1000);
+     }
+     
 
   }
 }
